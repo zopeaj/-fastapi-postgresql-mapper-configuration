cffd7dc5cb6bcd3cf34f677def0799dd94f17e38
Ezekiel <zopeaj@gmail.com>
2023-01-25 07:59:50 +0100

    Session and main updated

--- backend/app/db/Session.py
+++ backend/app/db/Session.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-POSTGRES_DATABASE_URL = "postgresql://postgres@password:postgresuser/testdb"
+POSTGRES_DATABASE_URL = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
 SQLITE_DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(SQLITE_DATABASE_URL, echo=True)
+
+engine = create_engine(POSTGRES_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
